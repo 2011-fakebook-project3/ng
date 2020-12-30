8f02e79101af4bab94afffd63bea4ccf88ea5ccc
Josiah Liberatore <josiahliberatore@gmail.com>
2020-12-30 14:27:23 -0800

    copy pasted nav bar from project 2 fakebook

--- fakebook/src/app/navbar-view/navbar-view.component.ts
+++ fakebook/src/app/navbar-view/navbar-view.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router'
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar-view',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar-view.component.css']
 })
 export class NavbarViewComponent implements OnInit {
+  isAuthenticated: boolean = false;
 
-  constructor() { }
+  searchName: string = '';
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+
   }
-
+  
+  login() {
+    console.log("login");
+  }
+  logout() {
+    console.log("logout");
+  }
+  onNotifySearch(name: any){
+    console.log(name)
+    this.router.navigate([`search/${name}`], { skipLocationChange: false })
+  }
 }
