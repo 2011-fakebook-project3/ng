6c6c174e16e0e01125c2079f849b0392cb3c233a
Oladipo Osinubi <diposinubi@gmail.com>
2020-12-30 15:15:54 -0500

    implemented test for auth service

--- fakebook/src/app/services/auth.service.spec.ts
+++ fakebook/src/app/services/auth.service.spec.ts
@@ -1,18 +1,39 @@
 import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { OktaAuthService } from '@okta/okta-angular';
+import { NEVER } from 'rxjs';
 
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
-  let Mock
+  let mockOktaAuthService = {
+    $authenticationState: NEVER
+  }
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: Router, useValue: {} },
+        { provide: OktaAuthService, useValue: mockOktaAuthService }
+      ]
+
+    });
     service = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+   
+
+  it('should return true if autheticated', () => {
+    expect(service.isAuthenticated()).toBeTruthy();
+  });
+
+  it('should return false if not autheticated', () => {
+    expect(service.isAuthenticated()).toBeFalsy();
+  });
+
 });
