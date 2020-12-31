c63153f19685a3b73afeff02ca3f94ec13f147c8
Josiah Liberatore <josiahliberatore@gmail.com>
2020-12-31 09:18:57 -0800

    initial create for notifications component and service

--- fakebook/src/app/app.module.ts
+++ fakebook/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotificationsViewComponent } from './notifications-view/notifications-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotificationsViewComponent
   ],
   imports: [
     BrowserModule,
