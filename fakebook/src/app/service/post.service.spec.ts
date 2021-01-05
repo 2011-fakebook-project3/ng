22a21a0e5099436956e55fe1a1f0f27f0b24f461
Joseph Ellis <josephtomellis@gmail.com>
2021-01-05 17:06:48 -0600

    updated service test to reflect new models

--- fakebook/src/app/service/post.service.spec.ts
+++ fakebook/src/app/service/post.service.spec.ts
@@ -20,12 +20,9 @@ describe('PostService', () => {
     lastName: 'last',
     email: 'e@mail',
     phoneNumber: undefined,
-    password: 'password',
     profilePictureUrl: null,
     status: undefined,
     birthDate: new Date(2010, 12),
-    followers: [],
-    followees: []
   };
 
   const testComment: Comment = {
@@ -33,9 +30,7 @@ describe('PostService', () => {
     userId: 1,
     content: 'comment content',
     postId: 1,
-    parentCommentId: undefined,
     createdAt: undefined,
-    childCommentIds: [],
     user: undefined
   };
 
