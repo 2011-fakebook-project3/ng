016efd8f578815e323cad1707cb8eafa6cac5de0
Oladipo Osinubi <diposinubi@gmail.com>
2021-01-04 21:13:29 -0500

    added hhtp client to test

--- fakebook/src/app/services/newsfeed.service.spec.ts
+++ fakebook/src/app/services/newsfeed.service.spec.ts
@@ -1,44 +1,62 @@
 import { TestBed } from '@angular/core/testing';
 import { Observable } from 'rxjs';
-import { Post } from '../models/post';
-import { User } from '../models/user';
+import { HttpClient, HttpErrorResponse, HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
+
+import { User } from '../models/user';
+import { Post } from '../models/post';
 import { NewsfeedService } from './newsfeed.service';
 
 describe('NewsfeedService', () => {
   let service: NewsfeedService;
+  let httpClientSpy: { get: jasmine.Spy, delete: jasmine.Spy, post: jasmine.Spy };
+  let httpTestingController: HttpTestingController;
+  const url = `testurl.net/api/Posts`;
 
-  let testUser: User = {
+  const testUser: User = {
     id: 1,
-    firstName: "Sherlock",
-    lastName:  "Holmes",
-    email: "sholmes@email.com",
+    firstName: 'Sherlock',
+    lastName: 'Holmes',
+    email: 'sholmes@email.com',
     phoneNumber: undefined,
-    password: "1234",
+    password: '1234',
     profilePictureUrl: null,
     status: undefined,
-    birthDate: new Date("2001-08-17"),
+    birthDate: new Date('2001-08-17'),
     followers: [],
     followees: []
-  }
+  };
 
-  let testPosts: Post[] = [
+  const testPosts: Post[] = [
     {
-      id: 1, 
-      content: "content 1",
+      id: 1,
+      content: 'content 1',
       user: testUser,
       createdAt: new Date(),
       likedByUserIds: [1, 2, 5],
-      pictureUrl: "",
+      pictureUrl: '',
       commentIds: [1, 2, 3],
       comments: [],
       liked: true
-    }
-  ]
+    },
+  ];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(NewsfeedService);
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [
+        NewsfeedService,
+      ]
+    });
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'delete', 'post']);
+
+    service = new NewsfeedService(httpClientSpy as any);
   });
 
   it('should be created', () => {
@@ -47,34 +65,63 @@ describe('NewsfeedService', () => {
 
   it('getPost should return list of posts',
     (done) => {
-      service.posts$.subscribe(value => {
-        expect(value).toBe(testPosts);
+      httpClientSpy.get.and.returnValue(testPosts);
+
+      service.getPosts()
+        .subscribe(post => {
+          expect(post).toEqual(testPosts);
+          done();
+        });
+
+      const req = httpTestingController.expectOne(`${url}`);
+
+      expect(req.request.method).toEqual('GET');
+      expect(req.request.body).toBeNull();
+      req.flush(testPosts);
+
+      httpTestingController.verify();
+    });
+
+  it('getPost should return content of post',
+    (done) => {
+      service.getPosts().subscribe(post => {
+        expect(post[0].content).toBe('content 1');
         done();
       });
+      const req = httpTestingController.expectOne(`${url}`);
+      expect(req.request.method).toBe('GET');
+      expect(req.request.body).toBeNull();
+      req.flush(testPosts);
+
+      httpTestingController.verify();
     });
 
-    it('getPost should return content of post', 
-      (done) => {
-      service.posts$.subscribe(value => {
-        expect(value[0].content).toBe("content 1");
-        done();
-      })
-    });
-
-    it('getPost should return user', 
+  it('getPost should return user',
     (done) => {
-      service.posts$.subscribe(value => {
-        expect(value[0].user).toBe(testUser);
+      service.getPosts().subscribe(post => {
+        expect(post[0].user).toBe(testUser);
         done();
-      })
+      });
+      const req = httpTestingController.expectOne(`${url}`);
+      expect(req.request.method).toBe('GET');
+      expect(req.request.body).toBeNull();
+      req.flush(testPosts);
+
+      httpTestingController.verify();
     });
 
-    it('getPost should return comment Ids', 
+  it('getPost should return comment Ids',
     (done) => {
-      service.posts$.subscribe(value => {
-        expect(value[0].commentIds[1]).toBe(2);
+      service.getPosts().subscribe(post => {
+        expect(post[0].commentIds[1]).toBe(2);
         done();
-      })
+      });
+      const req = httpTestingController.expectOne(`${url}`);
+      expect(req.request.method).toBe('GET');
+      expect(req.request.body).toBeNull();
+      req.flush(testPosts);
+
+      httpTestingController.verify();
     });
 
 
--- fakebook/src/app/services/newsfeed.service.ts
+++ fakebook/src/app/services/newsfeed.service.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Post } from '../models/post'
+import { HttpClient } from '@angular/common/http';
+import { Post } from '../models/post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsfeedService {
-  
-  constructor() { }
-  
-  get posts$(): Observable<Post[]>{
-    return this.getPostObservable();
-  }
-  
-  getPostObservable(): Observable<Post[]> {
+
+  constructor(private http: HttpClient) { }
+  baseUrl = 'someUrl';
+  url = `${this.baseUrl}/api/Posts`;
+
+  getPosts(): Observable<Post[]> {
     return undefined as unknown as Observable<Post[]>;
   }
-  
+
 }
