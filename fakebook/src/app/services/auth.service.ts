cc165a6f9c43071f9f4c8d9b9bd3eff1f8f6ff9a
David Barnes <david.barnes0402@gmail.com>
2021-01-06 16:23:59 -0500

    implementation of auth service.

--- fakebook/src/app/services/auth.service.ts
+++ fakebook/src/app/services/auth.service.ts
@@ -14,17 +14,31 @@ export class AuthService {
   isAuthenticated: boolean | unknown;
 
   constructor(public oktaAuth: OktaAuthService, public router: Router) {
+    this.updateAuthState(false);
+    this.oktaAuth.$authenticationState.subscribe((isAuthenticated) =>
+    this.updateAuthState(isAuthenticated)
+    );
   }
 
   updateAuthState(isAuthenticated: boolean): void {
+    this.isAuthenticated = isAuthenticated;
+    if (isAuthenticated) {
+      this.oktaAuth.getUser().then(console.log);
+    }
   }
 
   subscribeAuthStateChange(updateFn: (authState: boolean) => void): void {
+    this.oktaAuth.$authenticationState.subscribe((authState) => updateFn(authState));
   }
 
   login(): void {
+    this.oktaAuth.signInWithRedirect({
+      originalUri: 'newsfeed'
+    });
   }
 
   logout(): void {
+    this.oktaAuth.signOut();
+    this.oktaAuth.tokenManager.clear();
   }
 }
