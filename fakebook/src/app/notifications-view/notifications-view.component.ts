08a13726a884b8e4d554b07ec0cd1e3067f60a28
Josiah Liberatore <josiahliberatore@gmail.com>
2021-01-07 09:16:52 -0800

    fixed linting issues

--- fakebook/src/app/notifications-view/notifications-view.component.ts
+++ fakebook/src/app/notifications-view/notifications-view.component.ts
@@ -10,9 +10,11 @@ import { Notification, LikeNotification, PostNotification, CommentNotification, 
 export class NotificationsViewComponent implements OnInit {
 
   notifications: Notification[] | undefined;
-  notifications_view: boolean = false;
+  notificationsView = false;
 
-  constructor(private service: NotificationsService) { }
+  constructor(
+    private notifService: NotificationsService,
+    ) { }
 
   ngOnInit(): void {
     // for testing purposes only
@@ -21,58 +23,17 @@ export class NotificationsViewComponent implements OnInit {
       { userId: 2, postId: 1, type: 'Comment' },
       { userId: 3, type: 'Follow' },
       { userId: 4, postId: 2, type: 'Post' },
-      { userId: 1, postId: 1, type: 'Like' },
-      { userId: 2, postId: 1, type: 'Comment' },
-      { userId: 3, type: 'Follow' },
-      { userId: 4, postId: 2, type: 'Post' },
-      { userId: 1, postId: 1, type: 'Like' },
-      { userId: 2, postId: 1, type: 'Comment' },
-      { userId: 3, type: 'Follow' },
-      { userId: 4, postId: 2, type: 'Post' },
-      { userId: 1, postId: 1, type: 'Like' },
-      { userId: 2, postId: 1, type: 'Comment' },
-      { userId: 3, type: 'Follow' },
-      { userId: 4, postId: 2, type: 'Post' },
-      { userId: 1, postId: 1, type: 'Like' },
-      { userId: 2, postId: 1, type: 'Comment' },
-      { userId: 3, type: 'Follow' },
-      { userId: 4, postId: 2, type: 'Post' },
-      { userId: 1, postId: 1, type: 'Like' },
-      { userId: 2, postId: 1, type: 'Comment' },
-      { userId: 3, type: 'Follow' },
-      { userId: 4, postId: 2, type: 'Post' },
-      { userId: 1, postId: 1, type: 'Like' },
-      { userId: 2, postId: 1, type: 'Comment' },
-      { userId: 3, type: 'Follow' },
-      { userId: 4, postId: 2, type: 'Post' },
-      { userId: 1, postId: 1, type: 'Like' },
-      { userId: 2, postId: 1, type: 'Comment' },
-      { userId: 3, type: 'Follow' },
-      { userId: 4, postId: 2, type: 'Post' },
-      { userId: 1, postId: 1, type: 'Like' },
-      { userId: 2, postId: 1, type: 'Comment' },
-      { userId: 3, type: 'Follow' },
-      { userId: 4, postId: 2, type: 'Post' },
-      { userId: 1, postId: 1, type: 'Like' },
-      { userId: 2, postId: 1, type: 'Comment' },
-      { userId: 3, type: 'Follow' },
-      { userId: 4, postId: 2, type: 'Post' },
-      { userId: 1, postId: 1, type: 'Like' },
-      { userId: 2, postId: 1, type: 'Comment' },
-      { userId: 3, type: 'Follow' },
-      { userId: 4, postId: 2, type: 'Post' },
-      
     ];
   }
 
   getNotifications(): void {
-    this.service.notifications$.subscribe(notifs => {
+    this.notifService.notifications$.subscribe(notifs => {
       this.notifications = notifs;
     });
   }
 
   toggleNotifications(): void {
-    this.notifications_view = !this.notifications_view;
+    this.notificationsView = !this.notificationsView;
   }
 
 }
