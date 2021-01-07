8de4064471f860c8d5b01d2d9758edc345bcbf40
Oladipo Osinubi <diposinubi@gmail.com>
2021-01-07 11:18:03 -0500

    added newfeed service to implementation

--- fakebook/src/app/services/auth.service.ts
+++ fakebook/src/app/services/auth.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
+  getAccessToken() {
+    throw new Error('Method not implemented.');
+  }
 
   constructor() { }
 }
--- fakebook/src/app/services/newsfeed.service.ts
+++ fakebook/src/app/services/newsfeed.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Post } from '../models/post';
+import { environment} from '../../environments/environment'
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -10,9 +11,14 @@ import { AuthService } from '../services/auth.service';
 export class NewsfeedService {
 
   constructor(private oktaAuth: AuthService, private http: HttpClient) { }
-
+  url = `${environment.baseUrl}/someUrl`;
   getPosts(): Observable<Post[]> {
-    return undefined as unknown as Observable<Post[]>;
+    const accessToken = this.oktaAuth.getAccessToken();
+    const headers = {
+      Authorization: 'Bearer ' + accessToken,
+      Accept: 'application/json',
+    };
+    return this.http.get<Post[]>(`${this.url}/`, { headers });
   }
 
 }
