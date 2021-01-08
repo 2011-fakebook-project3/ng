65c0ccf697e133bee65c5c745c3872985e9bfeba
Oladipo Osinubi <diposinubi@gmail.com>
2021-01-08 11:08:42 -0500

    added getUser method to new feed component

--- fakebook/src/app/components/newsfeed/newsfeed.component.ts
+++ fakebook/src/app/components/newsfeed/newsfeed.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { Post } from '../../models/post';
 import { NewsfeedService } from '../../services/newsfeed.service';
 
 @Component({
@@ -8,9 +10,33 @@ import { NewsfeedService } from '../../services/newsfeed.service';
 })
 export class NewsfeedComponent implements OnInit {
 
-  constructor() { }
+   posts : Post[] = [];
+   user: User;
+
+   testposts:Post[] =[
+    { id: 1, content: 'content 1', createdAt: new Date(), pictureUrl: '', email: 'irene@email.com', comments: [] },
+    { id: 2, content: 'content 2', createdAt: new Date(), pictureUrl: '', email: 'moriarty@email.com', comments: [] }
+   ]
+
+  constructor(
+    private newsfeedService: NewsfeedService
+  ) { }
 
   ngOnInit(): void {
+    console.log(this.posts);
+    this.getPosts();
+    this.getUser();
+    console.log(this.posts);
+    console.log(this.user);
   }
 
+  getPosts(){
+    this.newsfeedService.getPosts()
+    .subscribe((gotPosts) => this.posts = gotPosts);
+  }
+
+  getUser(){
+    this.newsfeedService.getUser()
+    .subscribe((gotUser) => this.user = gotUser);
+  }
 }
