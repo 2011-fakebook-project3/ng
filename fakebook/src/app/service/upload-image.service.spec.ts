a53c1db736b9bd0a348c87302bf1653f17062d84
David Barnes <david.barnes0402@gmail.com>
2021-01-11 13:56:47 -0500

    add passing test to upload image service

--- fakebook/src/app/service/upload-image.service.spec.ts
+++ fakebook/src/app/service/upload-image.service.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { OktaAuthService } from '@okta/okta-angular';
 
 import { UploadImageService } from './upload-image.service';
 
@@ -6,11 +8,24 @@ describe('UploadImageService', () => {
   let service: UploadImageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const mockOktaAuthService = {
+      getAccessToken(): string {
+        return '';
+      }
+    };
+    const mockHttpClient = {};
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpClient, useValue: mockHttpClient },
+        { provide: OktaAuthService, useValue: mockOktaAuthService}
+      ]
+    }).compileComponents();
     service = TestBed.inject(UploadImageService);
   });
 
   it('should be created', () => {
+    service = new UploadImageService(TestBed.inject(HttpClient), TestBed.inject(OktaAuthService));
     expect(service).toBeTruthy();
+    expect(service.baseUrl).toBe('http://localhost:4200/api/profiles/');
   });
 });
