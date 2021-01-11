1ee0f47a44384611a66874b48a65cc3c8f49d48c
Jordan <jordaneddygarcia@gmail.com>
2021-01-11 14:09:09 -0600

    Initial Creation of User Profile
    * Created User View Component.
    * Added initial testing.
    * Created User Service to get http request.

--- fakebook/src/app/app-routing.module.ts
+++ fakebook/src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { OktaCallbackComponent } from '@okta/okta-angular';
 import { MainViewComponent } from './component/main-view/main-view.component';
 import { NewsfeedComponent } from './component/newsfeed/newsfeed.component';
+import { ProfileViewComponent } from './component/profile-view/profile-view.component';
 
 const routes: Routes = [
   {  path: '', component: MainViewComponent },
   { path: 'login/callback', component: OktaCallbackComponent },
   { path: 'logout', component: OktaCallbackComponent },
   { path: 'newsfeed', component: NewsfeedComponent },
+  { path: 'user/:id', component: ProfileViewComponent },
+
 ];
 
 @NgModule({
