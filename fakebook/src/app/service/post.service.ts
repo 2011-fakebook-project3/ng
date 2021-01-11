45ac9afe259a93775b8afbb6b905e5e612707aec
Joseph Ellis <josephtomellis@gmail.com>
2021-01-11 09:23:38 -0600

    added okta import

--- fakebook/src/app/service/post.service.ts
+++ fakebook/src/app/service/post.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { OktaCallbackComponent } from '@okta/okta-angular';
 
 import { Post } from '../model/post';
 
