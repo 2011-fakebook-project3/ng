dc6830afe7675aeb6d63b2e8def6a128c71ff3bd
Josiah Liberatore <josiahliberatore@gmail.com>
2021-01-12 12:23:36 -0800

    production environment URL updated

--- fakebook/src/environments/environment.prod.ts
+++ fakebook/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // update to correct url when services are in place
-  baseUrl: 'https://fakebook.revaturelabs.com/api/service'
+  baseUrl: 'https://fakebook.revaturelabs.com/api'
 };
