ec73c8d005c9421337719933655f439799de7b29
David Barnes <david.barnes0402@gmail.com>
2021-01-12 16:59:16 -0500

    add forgotten semi colon so that ng lint is now happy...

--- fakebook/src/app/component/profile-view/profile-view.component.ts
+++ fakebook/src/app/component/profile-view/profile-view.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'; // getting the id number
 import { User } from 'src/app/model/user';
-import { ProfileService } from '../../service/profile.service'
+import { ProfileService } from '../../service/profile.service';
 
 @Component({
   selector: 'app-profile-view',
