c93c62462af45b265ce1abdaf03a0d341ad986ad
dannydjg16 <dannydjg16@gmail.com>
2021-01-12 02:08:22 -0500

    added test for comment service

--- fakebook/src/app/service/comment.service.spec.ts
+++ fakebook/src/app/service/comment.service.spec.ts
@@ -1,16 +1,24 @@
-// import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { OktaAuthService } from '@okta/okta-angular';
 
-// import { CommentService } from './comment.service';
+import { CommentService } from './comment.service';
 
-// describe('CommentService', () => {
-//   let service: CommentService;
+describe('CommentService', () => {
+  let service: CommentService;
+  let fakeAuthService = { }
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(CommentService);
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+        imports: [HttpClientModule],
+        providers: [
+            { provide: OktaAuthService, useValue: fakeAuthService }
+        ]
+    });
+    service = TestBed.inject(CommentService);
+  });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
