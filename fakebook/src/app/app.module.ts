f6a77ff97797844c588c6ee9b58a245f265cacf2
Nick Escalona <nicholas.escalona@gmail.com>
2021-01-30 03:11:03 -0600

    update deps

--- fakebook/src/app/app.module.ts
+++ fakebook/src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Pipe, PipeTransform } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostViewComponent } from './component/post-view/post-view.component';
