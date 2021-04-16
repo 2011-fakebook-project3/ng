1d3f3ab9effd5f3a7ea269b9d6ab1db2bfc2705c
Bryson Ewell <brysonewell@outlook.com>
2021-04-16 17:44:23 -0400

    Fixed race condition for user variable.

--- fakebook/src/app/component/newsfeed/newsfeed.component.ts
+++ fakebook/src/app/component/newsfeed/newsfeed.component.ts
@@ -22,23 +22,26 @@ export class NewsfeedComponent implements OnInit {
     ) {}
 
   ngOnInit(): void {
-    this.getUser();
     this.route.params.subscribe((params) => {
-      if(params['id'] !== undefined) {
+      let loadUserFeed = params['id'] !== undefined;
+      this.getPosts(loadUserFeed);
+      if(!loadUserFeed) {
         this.postId = +params['id'];
         if(this.postId !== undefined && !isNaN(this.postId)) {
           this.getPostById();
         }
-      } else {
-        this.getPosts();
-      }
+      } 
     });
   }
 
-  getPosts(): void {
+  getPosts(loadUserFeed : boolean): void {
     this.newsfeedService
-      .getPosts(this.user?.followers!)
-      .subscribe((gotPosts) => (this.posts = gotPosts));
+      .getUser()
+      .subscribe((gotUser) => { 
+        this.user = gotUser;
+         this.newsfeedService.getPosts(gotUser?.followers!)
+         .subscribe((gotPosts) => (this.posts = gotPosts));
+      });
   }
 
   getPostById(): void {
@@ -47,12 +50,6 @@ export class NewsfeedComponent implements OnInit {
       .getPostById(this.postId)
       .subscribe((p) => this.posts = [p]);
     }
-  }
-
-  getUser(): void {
-    this.newsfeedService
-      .getUser()
-      .subscribe((gotUser) => (this.user = gotUser));
   }
 
   onNotifyComment(valueEmitted: any): any {
