9e7bc649549c6bc872980b217e95554ceebfefa8
Trevor Dunbar <tdunbar123@yahoo.com>
2021-04-19 11:56:18 -0400

    Fixed test

--- fakebook/src/app/component/profile-search/profile-search.component.spec.ts
+++ fakebook/src/app/component/profile-search/profile-search.component.spec.ts
@@ -1,21 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileSearchComponent } from './profile-search.component';
+import { ProfileService } from 'src/app/service/profile.service';
+import { HttpClientModule } from '@angular/common/http';
+import { OktaAuthService } from '@okta/okta-angular';
 
 describe('ProfileSearchComponent', () => {
   let component: ProfileSearchComponent;
   let fixture: ComponentFixture<ProfileSearchComponent>;
 
+  const fakeHTTPClient = {};
+  const fakeOktaAuth = { getAccessToken(): void {} };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProfileSearchComponent ]
+      imports: [HttpClientModule],
+      declarations: [ ProfileSearchComponent ],
+      providers: [
+        { provide: HttpClientModule, useValue: fakeHTTPClient },
+        { provide: OktaAuthService, useValue: fakeOktaAuth },
+      ],
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProfileSearchComponent);
-    component = fixture.componentInstance;
+    component = new ProfileSearchComponent(
+      TestBed.inject(ProfileService)
+    );
     fixture.detectChanges();
   });
 
