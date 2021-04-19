70cbb4dd135c7f3b86dd09add18b944ccd6c6adb
Andrew Park <modem428@gmail.com>
2021-04-18 18:37:23 -0700

    add tests for follow.service

--- fakebook/src/app/services/follow.service.spec.ts
+++ fakebook/src/app/services/follow.service.spec.ts
@@ -1,19 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { Observable, of } from 'rxjs';
 
 import { FollowService } from './follow.service';
 
 describe('FollowService', () => {
   let service: FollowService;
 
+  const mockHttpClient = {
+    post(url: string, body: any): Observable<any> {
+      return of(true);
+    }
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [{ provide: HttpClient, useValue: {} }],
+      providers: [{ provide: HttpClient, useValue: mockHttpClient }],
     });
     service = TestBed.inject(FollowService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should expect follow to not throw error', () => {
+    expect(() => { service.follow('jwerner@revature.net'); }).not.toThrow();
+  });
+
+  it('should expect unfollow to not throw error', () => {
+    expect(() => { service.unfollow('jwerner@revature.net'); }).not.toThrow();
   });
 });
